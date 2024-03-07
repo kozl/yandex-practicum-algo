7099126c72d04935efe27f24ac694e0be6d8b372
Aleksandr Kozlov <avlkozlov@avito.ru>
2024-03-07 23:50:29 +0300

    added solution

--- sprint7/e/main.go
+++ sprint7/e/main.go
@@ -3,19 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func solve(total int, denominaitons []int) (banknotesCount int) {
-	panic("not implemented")
+	dp := make([]int, total+1)
+	for i := 0; i <= total; i++ {
+		dp[i] = math.MaxInt32
+	}
+
+	for i := 1; i <= len(denominaitons); i++ {
+		for j := 1; j <= total; j++ {
+			if j-denominaitons[i-1] > 0 {
+				dp[j] = min(dp[j-denominaitons[i-1]]+1, dp[j])
+				continue
+			}
+			if j-denominaitons[i-1] == 0 {
+				dp[j] = 1
+			}
+		}
+	}
+	return dp[total]
 }
 
 func main() {
 	scanner := makeScanner()
 	total := readInt(scanner)
+	_ = readInt(scanner)
 	denominaitons := readArray(scanner)
+	sanitize(&denominaitons)
 	banknotesCount := solve(total, denominaitons)
 	fmt.Println(banknotesCount)
 }
@@ -43,4 +63,17 @@ func readInt(scanner *bufio.Scanner) int {
 	stringInt := scanner.Text()
 	res, _ := strconv.Atoi(stringInt)
 	return res
+}
+
+func sanitize(denominations *[]int) {
+	m := map[int]bool{}
+	for _, i := range *denominations {
+		m[i] = true
+	}
+	result := []int{}
+	for k := range m {
+		result = append(result, k)
+	}
+	sort.Ints(result)
+	*denominations = result
 }
--- sprint7/e/main_test.go
+++ sprint7/e/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"sort"
+	"testing"
+)
 
 func Test_solve(t *testing.T) {
 	type args struct {
@@ -18,10 +21,28 @@ func Test_solve(t *testing.T) {
 				total:         130,
 				denominaitons: []int{10, 3, 40, 1},
 			},
+			wantBanknotesCount: 4,
+		},
+		{
+			name: "example 2",
+			args: args{
+				total:         100,
+				denominaitons: []int{7, 5},
+			},
+			wantBanknotesCount: 16,
+		},
+		{
+			name: "example 3",
+			args: args{
+				total:         1,
+				denominaitons: []int{1},
+			},
+			wantBanknotesCount: 1,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			sort.Ints(tt.args.denominaitons)
 			if gotBanknotesCount := solve(tt.args.total, tt.args.denominaitons); gotBanknotesCount != tt.wantBanknotesCount {
 				t.Errorf("solve() = %v, want %v", gotBanknotesCount, tt.wantBanknotesCount)
 			}
