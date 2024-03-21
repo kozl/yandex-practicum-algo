b0aafb84bd4b575a892df8dbcf60b6711618b02f
Aleksandr Kozlov <avlkozlov@avito.ru>
2024-03-22 01:29:54 +0300

    optimization

--- sprint8/e/main.go
+++ sprint8/e/main.go
@@ -11,17 +11,19 @@ import (
 
 func solve(s string, operations []operation) string {
 	sort.Slice(operations, func(i, j int) bool { return operations[i].pos < operations[j].pos })
-	var acc int
-	result := s
+	var pos int
+	var result strings.Builder
 	for _, op := range operations {
-		result = insert(result, op.s, op.pos+acc)
-		acc += len(op.s)
+		for i := pos; i < op.pos; i++ {
+			result.WriteByte(s[i])
+		}
+		result.WriteString(op.s)
+		pos = op.pos
 	}
-	return result
-}
-
-func insert(s, sub string, pos int) string {
-	return s[:pos] + sub + s[pos:]
+	for i := pos; i < len(s); i++ {
+		result.WriteByte(s[i])
+	}
+	return result.String()
 }
 
 type operation struct {
